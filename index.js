0b55bf6960981f1ab9eae0baca649a1f52ffbe60
Juan Sebastian Lengyel <juan.lengyel@hotmail.com>
2020-03-11 16:32:48 -0500

    Implemented a simple RESTful web service

--- index.js
+++ index.js
@@ -4,15 +4,35 @@ const express = require('express');
 // Instance express as the main holder of the application
 const app = express();
 
-// The port that the application will bind to. Defined by env variables. If not, default to 3000
-const port = process.env.PORT || 3000;
+// Array used to store articles
+const articles = [{title: 'Yo Momma'}];
 
-// HTTP response to the most basic request
-app.get('/', (req, res) => {
-  res.send('Hello World');
+// Set 'port' as an application property. Defined by env variables. If not, default to 3000
+app.set('port', process.env.PORT || 3000);
+
+// GET HTTP method to retrieve all stored articles
+app.get('/articles', (req, res, next) => {
+  res.send('OK');
 });
 
-// Set the web app to listen on defined port
-app.listen(port, () => {
-  console.log(`Express web app available at localhost: ${port}`);
+// GET HTTP method to retrieve a single stored article
+app.get('/articles/:id', (req, res, next) => {
+  const id = req.params.id;
+  console.log('Fetching: ', id);
+  res.send(articles[id]);
 });
+
+// DELETE HTTP method to delete a single stored article
+app.delete('/articles/:id', (req, res, next) => {
+  const id = req.params.id;
+  console.log('Deleting: ', id);
+  delete articles[id];
+  res.send({ message: 'Delete' });
+});
+
+app.listen(app.get('port'), () => {
+  console.log('App started on port: ', app.get('port'));
+});
+
+
+module.exports = app;
