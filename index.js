44a2f2135d984ffe364991aa145ff9296d3594ae
Juan Sebastian Lengyel <juan.lengyel@hotmail.com>
2020-03-11 16:46:33 -0500

    Implemented POST method to push articles to the server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Import express to use it for the web application
 const express = require('express');
+// Import body-parser to interpret json incoming to the server
+const bodyParser = require('body-parser');
 
 // Instance express as the main holder of the application
 const app = express();
@@ -9,6 +11,12 @@ const articles = [{title: 'Yo Momma'}];
 
 // Set 'port' as an application property. Defined by env variables. If not, default to 3000
 app.set('port', process.env.PORT || 3000);
+
+// Support request bodies encoded as JSON
+app.use(bodyParser.json());
+
+// Support form encoded bodies
+app.use(bodyParser.urlencoded({ extended: true}));
 
 // GET HTTP method to retrieve all stored articles
 app.get('/articles', (req, res, next) => {
@@ -20,6 +28,13 @@ app.get('/articles/:id', (req, res, next) => {
   const id = req.params.id;
   console.log('Fetching: ', id);
   res.send(articles[id]);
+});
+
+// POST HTTP method to store a new article
+app.post('/articles', (req, res, next) => {
+  const article = { title: req.body.title };
+  articles.push(article);
+  res.send(article);
 });
 
 // DELETE HTTP method to delete a single stored article
